cc6131b84d4da41b2e6c490d6743c0d795cc7c12
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-01-04 20:12:17 -0300

    feat: utilize logger on main

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"github.com/gabriel-O-C/godin/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger := config.GetLogger("main")
 	err := config.Init()
 
 	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
 		return
 	}
 
