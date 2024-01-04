a293fe9fc3e890d6a8c36554917fd7669ee7d8b4
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-01-04 19:59:15 -0300

    feat: create logger

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/gabriel-O-C/godin/router"
+import (
+	"github.com/gabriel-O-C/godin/config"
+	"github.com/gabriel-O-C/godin/router"
+)
 
 func main() {
+	err := config.Init()
+
+	if err != nil {
+		return
+	}
+
 	router.Initialize()
 }
