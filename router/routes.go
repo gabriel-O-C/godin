a271825971923c20c02f319b60bbd0f9d14903ef
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-01-03 22:34:49 -0300

    wip

---
+++ router/routes.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(r *gin.Engine) {
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "GET opening",
+			})
+		})
+	}
+
+}
