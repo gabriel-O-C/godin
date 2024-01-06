4b2175673cc5aea38065b75b6e750e07554af35c
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-01-06 15:29:23 -0300

    feat: add delete route

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gabriel-O-C/godin/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "query parameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	err := db.First(&opening, id).Error
+	if err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("id %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "Error on deleting opening")
+		return
+	}
+
+	sendSuccess(ctx, "delete opening", opening)
 }
