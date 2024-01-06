0e05abe87ac48d96cf222a89ceecbdb563efb401
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-01-05 21:26:34 -0300

    feat: add create opening

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"github.com/gabriel-O-C/godin/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+
+	db = config.GetDb()
+}
